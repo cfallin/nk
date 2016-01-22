bc86eda7b99d626d2746da7e6554cce2eb385570
Chris Fallin <cfallin@c1f.net>
2016-01-22 17:18:09 -0500

    Databuf tests.

--- kernel.h
+++ kernel.h
@@ -20,7 +20,7 @@ void nk_init();
 #define NK_ALLOCBYTES(type, n) ((type *)calloc((n), 1))
 #define NK_FREE(ptr) free(ptr)
 
-static inline void __nk_free_cleanup(void *p) { NK_FREE(p); }
+static inline void __nk_free_cleanup(void *p) { NK_FREE(*(void **)p); }
 #define NK_AUTOPTR(type) __attribute__((cleanup(__nk_free_cleanup))) type *
 #define NK_AUTOPTR_STEAL(type, p)                                              \
   ({                                                                           \
--- test_databuf.c
+++ test_databuf.c
@@ -1,6 +1,44 @@
 #include "test.h"
 #include "databuf.h"
 
-NK_TEST(databuf) {
+NK_TEST(databuf_alloc) {
+    nk_databuf* b;
+
+    NK_TEST_ASSERT(nk_databuf_new(&b, 8) == NK_OK);
+    NK_TEST_ASSERT(!NK_DATABUF_ISLONG(b));
+    NK_TEST_ASSERT(!NK_DATABUF_ISALLOC(b));
+    NK_TEST_ASSERT(NK_DATABUF_CAP(b) == 8);
+    NK_TEST_ASSERT(NK_DATABUF_LEN(b) == 0);
+    nk_databuf_free(b);
+
+    NK_TEST_ASSERT(nk_databuf_new(&b, 32) == NK_OK);
+    NK_TEST_ASSERT(NK_DATABUF_ISLONG(b));
+    NK_TEST_ASSERT(!NK_DATABUF_ISALLOC(b));
+    NK_TEST_ASSERT(NK_DATABUF_CAP(b) == 32);
+    NK_TEST_ASSERT(NK_DATABUF_LEN(b) == 0);
+    NK_TEST_ASSERT(NK_DATABUF_DATA(b) == b + 1);
+    nk_databuf_free(b);
+
+    NK_TEST_OK();
+}
+
+NK_TEST(databuf_reserve) {
+    nk_databuf* b;
+
+    NK_TEST_ASSERT(nk_databuf_new(&b, 0) == NK_OK);
+    NK_TEST_ASSERT(NK_DATABUF_CAP(b) == 0);
+
+    NK_TEST_ASSERT(nk_databuf_reserve(b, 8) == NK_OK);
+    NK_TEST_ASSERT(NK_DATABUF_CAP(b) == 8);
+    NK_TEST_ASSERT(NK_DATABUF_LEN(b) == 0);
+    NK_TEST_ASSERT(!NK_DATABUF_ISLONG(b));
+    NK_TEST_ASSERT(!NK_DATABUF_ISALLOC(b));
+
+    NK_TEST_ASSERT(nk_databuf_reserve(b, 32) == NK_OK);
+    NK_TEST_ASSERT(NK_DATABUF_CAP(b) == 32);
+    NK_TEST_ASSERT(NK_DATABUF_LEN(b) == 0);
+    NK_TEST_ASSERT(NK_DATABUF_ISLONG(b));
+    NK_TEST_ASSERT(NK_DATABUF_ISALLOC(b));
+
     NK_TEST_OK();
 }
